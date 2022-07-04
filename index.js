42c12c54293cddb7719652ce0c66d3ba5fb6c9f4
sukanyagovindan <gsukanya@gmail.com>
2022-07-04 16:22:07 -0700

    submit assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+
+
+function superbowlWin (somearray) {
+         
+const result = somearray.find(didtheywin);
+console.log(result);
+if (result) {
+    return result["year"];
+}
+
+
+
+}
+
+function didtheywin(element) {
+    console.log(element["year"],element["result"]);
+    if (element["result"]==="W") {
+        console.log("Here" + element["year"]);
+        return true;
+    }
+}
+
+
+
+
